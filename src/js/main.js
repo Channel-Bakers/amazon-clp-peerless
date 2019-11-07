5768fe38c752313c26189daa4233f17cf5e7d44b
Ryan DiMascio <ryan.dimascio@gmail.com>
2019-11-07 11:19:40 -0800

    set a timeout to wait for polyfill to load

--- src/js/main.js
+++ src/js/main.js
@@ -5,54 +5,56 @@ import Builder from './util/Builder';
 import {getCookie} from './util/helpers/cookies';
 
 (() => {
-	if (window.location.host === 'advertising.amazon.com') return;
+	setTimeout(() => {
+		if (window.location.host === 'advertising.amazon.com') return;
 
-	let CB = {};
-	window.CB = CB;
-	CB.sessionID = getCookie('session-id');
+		let CB = {};
+		window.CB = CB;
+		CB.sessionID = getCookie('session-id');
 
-	const slimSuitJackets = jackets.default;
-	const slimSuitPants = pants.default;
+		const slimSuitJackets = jackets.default;
+		const slimSuitPants = pants.default;
 
-	const slimSuitOptions = {
-		target: 'slimSuit',
-		title: `Slim Fit {{COLOR}} Suit`,
-		caption:
-			"Slim, polished, and versatile enough for just about any occasion. This handsome grey Lazio suit is cut from pure S110's wool by Vitale Barberis Canonico.",
-		image: {
-			position: 'left',
-		},
-		colors: [
-			{
-				name: 'charcoal',
-				hex: '#454f54',
-				image:
-					'https://images-na.ssl-images-amazon.com/images/I/61grhh2qwmL._AC_UY741_.jpg',
-				active: true,
+		const slimSuitOptions = {
+			target: 'slimSuit',
+			title: `Slim Fit {{COLOR}} Suit`,
+			caption:
+				"Slim, polished, and versatile enough for just about any occasion. This handsome grey Lazio suit is cut from pure S110's wool by Vitale Barberis Canonico.",
+			image: {
+				position: 'left',
 			},
-			{
-				name: 'black',
-				hex: '#000000',
-				image:
-					'https://images-na.ssl-images-amazon.com/images/I/61NhWRvi5eL._AC_SY679._SX._UX._SY._UY_.jpg',
-				active: false,
-			},
-		],
-		dropdowns: [
-			{
-				title: `Slim Fit {{COLOR}} Jacket`,
-				id: 'slimSuitJackets',
-				data: slimSuitJackets,
-			},
-			{
-				title: `Slim Fit {{COLOR}} Pants`,
-				id: 'slimSuitPants',
-				data: slimSuitPants,
-			},
-		],
-	};
+			colors: [
+				{
+					name: 'charcoal',
+					hex: '#454f54',
+					image:
+						'https://images-na.ssl-images-amazon.com/images/I/61grhh2qwmL._AC_UY741_.jpg',
+					active: true,
+				},
+				{
+					name: 'black',
+					hex: '#000000',
+					image:
+						'https://images-na.ssl-images-amazon.com/images/I/61NhWRvi5eL._AC_SY679._SX._UX._SY._UY_.jpg',
+					active: false,
+				},
+			],
+			dropdowns: [
+				{
+					title: `Slim Fit {{COLOR}} Jacket`,
+					id: 'slimSuitJackets',
+					data: slimSuitJackets,
+				},
+				{
+					title: `Slim Fit {{COLOR}} Pants`,
+					id: 'slimSuitPants',
+					data: slimSuitPants,
+				},
+			],
+		};
 
-	const slimSuitBuilder = new Builder({
-		...slimSuitOptions,
-	});
+		const slimSuitBuilder = new Builder({
+			...slimSuitOptions,
+		});
+	}, 500);
 })();
